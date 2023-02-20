6effeb9aa203c782fa51aa4891841605ed0cb4f7
kevinmcsherry <kevinmcsherry01@gmail.com>
2023-02-20 12:25:27 +0000

    Creating basic list structure and view function

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import shopList
+from .views import TaskList, TaskDetail
 
 
 urlpatterns = [
-    path('', shopList.as_view(), name='lists')
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
 ]
