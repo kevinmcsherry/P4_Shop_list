285f984de6e07a673fa875f2863bb3fd41080f2a
kevinmcsherry <kevinmcsherry01@gmail.com>
2023-02-20 15:10:29 +0000

    Added an update function and linked to edit form

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate
 
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
-    path('create-item/', TaskCreate.as_view(), name='create_item'),
+    path('create-item/', TaskCreate.as_view(), name='create-item'),
+    path('edit-item/<int:pk>/', TaskUpdate.as_view(), name='edit-item'),
+
 ]
