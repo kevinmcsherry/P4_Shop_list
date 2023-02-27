7ad3bd2f2f4a86b9b476f8250b8bdd07d654a764
kevinmcsherry <kevinmcsherry01@gmail.com>
2023-02-27 20:22:05 +0000

    Styling the pages and making links more efficient

--- shop_app/views.py
+++ shop_app/views.py
@@ -46,6 +46,7 @@ class TaskList(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
+        
 
         search_input = self.request.GET.get('Search-field') or ''
         if search_input:
