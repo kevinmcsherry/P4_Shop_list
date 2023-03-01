ead14affb52caf3c84a253a7825cb408c18f4d96
kevinmcsherry <kevinmcsherry01@gmail.com>
2023-03-01 03:39:14 +0000

    Adding further styling to shop list page

--- shop_app/views.py
+++ shop_app/views.py
@@ -46,6 +46,7 @@ class TaskList(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
+        #context['count'] = context['tasks'].filter(user=self.request.user)
         
 
         search_input = self.request.GET.get('Search-field') or ''
