689aa9890e5834d2a6486c1da356194bdeb5bc4e
kevinmcsherry <kevinmcsherry01@gmail.com>
2023-03-15 10:16:23 +0000

    Created new secret key and added to env file

--- P4_shopping_list/settings.py
+++ P4_shopping_list/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!)ea4#hu(7n*9#c2k$b!qoy(it+=b-l^po&&n)h_07*lj9h$%5'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,6 +85,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # Password validation
