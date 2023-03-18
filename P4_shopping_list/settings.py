bc3c7d670e95dfe87fbf2409722cf913cc00ab57
kevinmcsherry <kevinmcsherry01@gmail.com>
2023-03-18 12:03:24 +0000

    Added Procfile

--- P4_shopping_list/settings.py
+++ P4_shopping_list/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "^km0nhwn9coa68lwr30kvp(1qiml8za%z5gwl163l0@+n*-om7"
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
